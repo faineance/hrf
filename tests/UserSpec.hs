d940d42dac2156e7a05a3f9b5c22070b564d1dce
faineance <faineance@users.noreply.github.com>
2018-09-23 16:51:33 +0100

    broken tests

--- tests/UserSpec.hs
+++ tests/UserSpec.hs
@@ -1,7 +1,23 @@
 
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module UserSpec where
-
+import Protolude
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances  ()
+import API
 import           Test.Hspec
+import           Servant.Swagger.Test
+import Database.Esqueleto
 
 
+instance Arbitrary User where
+  arbitrary = User <$> arbitrary <*> arbitrary
+
+instance Arbitrary a => Arbitrary (Entity a) where
+  arbitrary = Entity <$> (return (toSqlKey 1))<*> arbitrary
+
+spec :: Spec
+spec = describe "Swagger" $ do
+  context "ToJSON matches ToSchema" $ validateEveryToJSON  appAPI
